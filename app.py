c48ce85daa8fd1db6db61d935890100bf5fe1c74
mumakkiyah <mmakkiyah@gmail.com>
2023-07-14 12:23:29 +0000

    added dynamic data and api route

--- app.py
+++ app.py
@@ -1,13 +1,49 @@
 # import Flask class from flask module
-from flask import Flask, render_template  
+from flask import Flask, render_template, jsonify
 # app is an object of the Flask class. Importing functionality from the module
-app = Flask(__name__)  
+app = Flask(__name__)
+
+JOBS = [{
+  'id': 1,
+  'title': 'Data Analyst',
+  'location': 'Singapore',
+}, {
+  'id': 2,
+  'title': 'Front End Engineer',
+  'location': 'Remote, USA',
+  'salary': '6600 USD'
+}, {
+  'id': 3,
+  'title': 'Database Administrator',
+  'location': 'Ho Chi Minh, Vietnam',
+  'salary': '6300 USD'
+}, {
+  'id': 4,
+  'title': 'Backend Engineer',
+  'location': 'Remote, Denmark',
+  'salary': '44,546 DKK'
+}, {
+  'id': 4,
+  'title': 'Data Scientist',
+  'location': 'Sydney, Australia',
+  'salary': '9500 AUD'
+}]
+
 
 # path after the / in the domain name
 # pass in the name of the main page to render
-@app.route("/")  
+@app.route("/")
 def hello_world():
-  return render_template('home.html') 
+  # pass the list of jobs and company into home.html as an argument to be dynamic in home.html using {{}}
+  return render_template('home.html', jobs=JOBS, company='Maple Leaf')
+
+
+# instead of returning a list, now we get the list in json format like in any API
+# we create a second route /jobs in the URL
+# it is an endpoint to return the data to requester for consumption in json and not only HTML
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 
 ### for testing only

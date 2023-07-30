a56446fb4f05bb0779e5d9a0e421249ea3321419
mumakkiyah <mmakkiyah@gmail.com>
2023-07-30 12:21:03 +0000

    dynamic pages for each job

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # import Flask class from flask module
 from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db
+from database import load_jobs_from_db, load_job_from_db
 
 # app is an object of the Flask class. Importing functionality from the module
 app = Flask(__name__)
@@ -9,10 +9,10 @@ app = Flask(__name__)
 # path after the / in the domain name
 # pass in the name of the main page to render
 @app.route("/")
-def hello_maple_leaf():
+def maple_leaf_index():
   jobs = load_jobs_from_db()
   # pass the list of jobs and company into home.html as an argument to be dynamic in home.html using {{}}
-  return render_template('home.html', jobs=jobs, company='Maple Leaf')
+  return render_template('home.html', jobs=jobs)
 
 
 # instead of returning a list, now we get the list in json format like in any API
@@ -22,6 +22,15 @@ def hello_maple_leaf():
 def list_jobs():
   jobs = load_jobs_from_db()
   return (jsonify(jobs))
+
+
+# a view for each job. The <id> is to make passing job id dynamic in flask
+@app.route("/job/<id>")
+def view_job(id):
+  job = load_job_from_db(id)
+  if not job:
+    return "Not found", 404
+  return render_template('jobpage.html', job=job)
 
 
 ### for testing only
--- database.py
+++ database.py
@@ -19,3 +19,16 @@ def load_jobs_from_db():
     for row in result.all():
       jobs.append(dict(row._mapping))
     return (jobs)
+
+
+# :val is string formatting in sqlalchemy to specify something that needs to be filled in and it comes from te URL in the path in app.py when user clicks to view
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(text(f"SELECT * FROM jobs WHERE id={id}"))
+    rows = []
+    for row in result.all():
+      rows.append(row._mapping)
+    if len(rows) == 0:
+      return None
+    else:
+      return dict(rows[0])
